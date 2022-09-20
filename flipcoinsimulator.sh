0f4c709c2cc1a9d9612aa24b13f9730123e729d5
Rakesh <byreddyrakesh41@gmail.com>
2022-09-20 23:18:03 +0530

    day10

--- flipcoinsimulator.sh
+++ flipcoinsimulator.sh
@@ -6,31 +6,71 @@ IS_TAIL=1
 headNumber=0
 tailNumber=0
 WINING_NUMBER=21
+isTie=0
 
-while [ $headNumber -lt $WINING_NUMBER ] && [ $tailNumber -lt $WINING_NUMBER ]
-do
-	randomValue=$((RANDOM%2))
-	if [ $randomValue -eq $IS_HEAD ]
-	then
-		((headNumber++))
-		echo "Cong. you got Head"
+function getRandomValue(){
+		randomValue=$((RANDOM%2))
+		if [ $randomValue -eq $IS_HEAD ]
+		then
+			((head_Number++))
+			echo "Cong. you got Head"
+		else
+			((tail_Number++))
+			echo "You got tails"
+		fi
+		if [ $tie -eq 1 ]
+		then
+			if [ $(($head_Number - $tail_Number)) -eq 2 ]
+			then
+				#To stop iteration
+				return `false`
+			fi
+		fi
+}
+
+function checkHeadTail(){
+	local head_Number=$1
+	local winingNumber=$2
+	local tail_Number=$3
+	local tie=$4
+	local breakLoop=true
+	if [ $tie -eq 0 ]
+	then 
+		while [ $head_Number -lt $winingNumber ] && [ $tail_Number -lt $winingNumber ]
+		do
+			getRandomValue
+		done
 	else
-		((tailNumber++))
-		echo "You got tails"
+		while $breakLoop
+		do
+			breakLoop=getRandomValue
+		done
 	fi
-	((totalFlip--))
-done
+	headNumber=$head_Number
+	tailNumber=$tail_Number
+}
+
+function winConditionFunction(){
+	local headValue=$1
+	local tailValue=$2
+if [ $headValue -gt $tailValue ]
+	then
+		echo "Head won by "$(($headValue - $tailValue))
+	else
+		echo "Tail won by "$(($tailValue - $headValue))
+fi
+}
+
+
+checkHeadTail $headNumber $WINING_NUMBER $tailNumber $isTie
 
 if [ $headNumber -eq $tailNumber ]
 then
 	echo "Tie"
-elif [ $headNumber -gt $tailNumber ]
-	then
-		echo "Head won by "$(($headNumber - $tailNumber))
-	else
-		echo "Tail won by "$(($tailNumber - $headNumber))
+	isTie=1
+	checkHeadTail $headNumber $WINING_NUMBER $tailNumber $isTie
+	echo "After tie"
+	winConditionFunction $headNumber $tailNumber
+else
+	winConditionFunction $headNumber $tailNumber
 fi
-
-#echo "Number of time head " $headNumber
-#echo "Number of time tails " $tailNumber
-
