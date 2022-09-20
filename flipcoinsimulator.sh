6f264f70bf0ac1c861b9a6b0c58c8337bdbd7dc9
Rakesh <byreddyrakesh41@gmail.com>
2022-09-20 23:14:56 +0530

    day10

--- flipcoinsimulator.sh
+++ flipcoinsimulator.sh
@@ -3,12 +3,23 @@
 randomValue=0
 IS_HEAD=0
 IS_TAIL=1
+headNumber=0
+tailNumber=0
+totalFlip=20
 
-randomValue=$((RANDOM%2))
+while [ $totalFlip -gt 0 ]
+do
+	randomValue=$((RANDOM%2))
+	if [ $randomValue -eq $IS_HEAD ]
+	then
+		((headNumber++))
+		echo "Cong. you got Head"
+	else
+		((tailNumber++))
+		echo "You got tails"
+	fi
+	((totalFlip--))
+done
 
-if [ $randomValue -eq $IS_HEAD ]
-then
-	echo "Cong. you got Head"
-else
-	echo "You got tails"
-fi
+echo "Number of time head " $headNumber
+echo "Number of time tails " $tailNumber
